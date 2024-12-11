eafe20e193057bdc7e7bcccd2090d7f7f0643008
shourya-r <rajputshourya7@gmail.com>
2024-12-11 16:11:34 +0530

    Added useId hook for generating unique id

--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -1,3 +1,5 @@
+import { useId } from "react";
+
 function InputBox({
   label,
   amount,
@@ -9,11 +11,18 @@ function InputBox({
   currencyDisable = false,
   className = "",
 }) {
+  const amountInputId = useId();
   return (
     <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
       <div className="w-1/2">
-        <label className="text-black/40 mb-2 inline-block">{label}</label>
+        <label
+          className="text-black/40 mb-2 inline-block"
+          htmlFor={amountInputId}
+        >
+          {label}
+        </label>
         <input
+          id={amountInputId}
           className="outline-none w-full bg-transparent py-1.5"
           type="number"
           placeholder="Amount"
